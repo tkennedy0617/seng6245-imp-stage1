d4fe409a71f486f5a66a01c15cd48ae12e4342b1
tkennedy0617 <tkennedy0617@gmail.com>
2015-01-28 17:18:23 -0500

    expression test cases
    package edu.ecu.cs.sle.imp.ast = 100% coverage

--- test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
+++ test/edu/ecu/cs/sle/imp/interpreter/InterpreterTest.java
@@ -21,4 +21,29 @@ public class InterpreterTest {
 		IntegerValue iv = interpreter.interpret("1*2");
 		assertEquals("1*2", 2, iv.getIntValue().intValue());
 	}	
+	
+	@Test
+	public void testAddition() {
+		IntegerValue iv = interpreter.interpret("3+4");
+		assertEquals("3+4", 7, iv.getIntValue().intValue());
+	}
+	
+	@Test
+	public void testSubtraction() {
+		IntegerValue iv = interpreter.interpret("6-5");
+		assertEquals("6-5", 1, iv.getIntValue().intValue());
+	}
+	
+	@Test
+	public void testDivison() {
+		IntegerValue iv = interpreter.interpret("4/2");
+		assertEquals("4/2", 2, iv.getIntValue().intValue());
+	}
+	
+	@Test
+	public void testUnaryMinus() {
+		IntegerValue iv = interpreter.interpret("-1");
+		assertEquals("-1", -1, iv.getIntValue().intValue());
+	}	
+	
 }
